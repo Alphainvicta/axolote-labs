82878a335c197ff1b0cb1059893ac2692ddb8067
Arlex Buenrostro <labarlex12@gmail.com>
2023-04-11 23:55:49 -0600

    Cambio de imagenes

--- src/sections/logos_bar/logos_bar.jsx
+++ src/sections/logos_bar/logos_bar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./logos_bar_style.scss";
 import { ReactComponent as Lreact } from "../../icons/REACT.svg";
 import { ReactComponent as Js } from "../../icons/JS.svg";
+import { ReactComponent as Postgre } from "../../icons/POSTGRE.svg";
 import { ReactComponent as Node } from "../../icons/NODE.svg";
 import { ReactComponent as Firebase } from "../../icons/FIREBASE.svg";
 import { ReactComponent as Excel } from "../../icons/EXCEL.svg";
@@ -13,6 +14,7 @@ const Logos_bar = () => {
         <Lreact />
         <Js />
         <Node />
+        <Postgre />
         <Firebase />
         <Excel />
       </div>
