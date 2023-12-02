be5a3fcdc62a64d31c996ecbf75a47c5858d5ba3
unknown <alphaceves@gmail.com>
2023-12-02 17:03:04 -0600

    form

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./input_style.scss";
 
-const Input = ({ input_type, input_icon, input_text }) => {
+const Input = ({ input_type, input_icon, input_text, onChange }) => {
   return (
     <div className="input_cont">
       <div className="input_icon">{input_icon}</div>
-      <input type={input_type} placeholder={input_text} />
+      <input
+        type={input_type}
+        name={input_type}
+        placeholder={input_text}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact_style.scss";
 import { ReactComponent as Phone } from "../../icons/TELEFONO.svg";
 import { ReactComponent as Mail } from "../../icons/CORREO.svg";
 import { ReactComponent as User } from "../../icons/User.svg";
 import { ReactComponent as Purple_mail } from "../../icons/Correo_morado.svg";
-
 import Button from "../../components/button/button";
 import { Button_class } from "../../components/button/button.jsx";
-
 import Input from "../../components/input/input";
 
 const Contact = () => {
-  function handleClick() {
-    console.log("El botón ha sido clickeado");
-  }
+  const [nameData, setNameData] = useState("");
+  const [emailData, setEmailData] = useState("");
+  const [messageData, setMessageData] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      nameData,
+      emailData,
+      messageData,
+    };
+    // Convert form data to JSON
+    const jsonData = JSON.stringify(formData, null, 2);
+    // Save jsonData to a file or use it as needed
+    console.log(jsonData);
+    fetch("http://192.168.1.99:80/api/", { method: "POST", body: jsonData });
+  };
 
   return (
     <div id="contact" className="contact_container">
@@ -32,25 +45,26 @@ const Contact = () => {
           <p>AxolotLabsContacto@gmail.com</p>
         </div>
       </div>
-      <div className="side_b">
+      <form className="side_b" onSubmit={handleSubmit}>
         <Input
           input_type="text"
           input_icon={<User />}
           input_text="Nombre completo"
+          onChange={(e) => setNameData(e.target.value)}
         />
         <Input
           input_type="email"
           input_icon={<Purple_mail />}
           input_text="Email"
+          onChange={(e) => setEmailData(e.target.value)}
         />
-        <textarea placeholder="Mensaje" />
-        <Button
-          text="Enviar"
-          button_select={Button_class.pink}
-          event={handleClick}
+        <textarea
+          name="Message"
+          onChange={(e) => setMessageData(e.target.value)}
+          placeholder="Mensaje"
         />
-        <button onClick={handleClick}>Haz clic aquí</button>
-      </div>
+        <Button text="Enviar" button_select={Button_class.pink} />
+      </form>
     </div>
   );
 };
