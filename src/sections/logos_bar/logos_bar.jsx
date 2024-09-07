fad58f021ffa44b56b986e3a2b37c4784dfa6aa2
unknown <alphaceves@gmail.com>
2024-09-07 16:08:03 -0600

    minor tweeks

--- src/sections/logos_bar/logos_bar.jsx
+++ src/sections/logos_bar/logos_bar.jsx
@@ -3,8 +3,8 @@ import "./logos_bar_style.scss";
 import { ReactComponent as Lreact } from "../../icons/REACT.svg";
 import { ReactComponent as Js } from "../../icons/JS.svg";
 import { ReactComponent as Postgre } from "../../icons/POSTGRE.svg";
-import { ReactComponent as Node } from "../../icons/NODE.svg";
-import { ReactComponent as Firebase } from "../../icons/FIREBASE.svg";
+import { ReactComponent as Php } from "../../icons/php-logo.svg";
+import { ReactComponent as Python } from "../../icons/python.svg";
 import { ReactComponent as Excel } from "../../icons/EXCEL.svg";
 
 const LogosBar = () => {
@@ -13,9 +13,9 @@ const LogosBar = () => {
       <div className="logos">
         <Lreact />
         <Js />
-        <Node />
+        <Php />
         <Postgre />
-        <Firebase />
+        <Python />
         <Excel />
       </div>
     </div>
