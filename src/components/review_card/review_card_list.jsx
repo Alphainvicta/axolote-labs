2baf88891753a8a7449167bbfec2403f002e0910
unknown <alphaceves@gmail.com>
2024-09-07 17:59:07 -0600

    tweeks

--- src/components/review_card/review_card_list.jsx
+++ src/components/review_card/review_card_list.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReviewCard from "./review_card";
 import { ReactComponent as ReviewImage1 } from "../../icons/Review.svg";
+import { ReactComponent as ReviewImage2 } from "../../icons/UserCirclePurple.svg";
+import { ReactComponent as ReviewImage3 } from "../../icons/UserCirclePink.svg";
 import { ReactComponent as Star5 } from "../../icons/5ESTRELLAS.svg";
 import { ReactComponent as Star4 } from "../../icons/4ESTRELLAS.svg";
 
@@ -19,7 +21,7 @@ const ReviewCardList = () => {
       id: "card1",
     },
     {
-      review_icon: <ReviewImage1 />,
+      review_icon: <ReviewImage2 />,
       review_name: t("reviews.sideb.card2.name"),
       review_description: t("reviews.sideb.card2.description"),
       review_text: t("reviews.sideb.card2.text"),
@@ -27,7 +29,7 @@ const ReviewCardList = () => {
       id: "card2",
     },
     {
-      review_icon: <ReviewImage1 />,
+      review_icon: <ReviewImage3 />,
       review_name: t("reviews.sideb.card3.name"),
       review_description: t("reviews.sideb.card3.description"),
       review_text: t("reviews.sideb.card3.text"),
